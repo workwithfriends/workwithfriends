ba3eefc3740411cbb4c21e292007781e58f762e9
demitri <demitri@mit.edu>
2014-04-05 16:18:13 -0400

    added loginWithFacebook url

--- workwithfriends.dreamhosters.com/wwf/wwf/urls.py
+++ workwithfriends.dreamhosters.com/wwf/wwf/urls.py
@@ -1,15 +1,17 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'wwf.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+                       # Examples:
+                       # url(r'^$', 'wwf.views.home', name='home'),
+                       # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^test/', 'wwf.views.test'),
-    url(r'^facebook/', include('django_facebook.urls')),
-    url(r'^accounts/', include('django_facebook.auth_urls')),
-                       )
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^test/', 'wwf.views.test'),
+                       url(r'^facebook/', include('django_facebook.urls')),
+                       url(r'^accounts/', include('django_facebook.auth_urls')),
+                       url(r'^loginWithFacebook/', include('wwf.views.loginWithFacebook')),
+    )
