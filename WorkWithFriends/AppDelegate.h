3400a292091d3d88e3e431a7f5a35c0e7aa0fea7
Jeremy <jw2016@mit.edu>
2014-04-05 13:37:04 -0400

    Initial Commit

---
+++ WorkWithFriends/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WorkWithFriends
+//
+//  Created by Jeremy Wohlwend on 4/5/14.
+//  Copyright (c) 2014 Jeremy Wohlwend. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WorkWithFriends/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  WorkWithFriends
+//
+//  Created by Jeremy Wohlwend on 4/5/14.
+//  Copyright (c) 2014 Jeremy Wohlwend. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
