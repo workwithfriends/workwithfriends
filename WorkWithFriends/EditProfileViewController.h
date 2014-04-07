ba653689da0ad384846b1c414d15ddccd70f3b02
Jeremy <jw2016@mit.edu>
2014-04-07 16:48:30 -0400

    Added the edit page for profile

---
+++ WorkWithFriends/EditProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditProfileViewController.h
+//  WorkWithFriends
+//
+//  Created by Jeremy Wohlwend on 4/7/14.
+//  Copyright (c) 2014 Jeremy Wohlwend. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditProfileViewController : UIViewController
+
+@end
