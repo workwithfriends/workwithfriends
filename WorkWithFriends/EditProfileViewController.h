d8a424ce1c26f8ee6ed938c019eadae0bfcbdc3e
Jeremy <jw2016@mit.edu>
2014-04-07 17:46:06 -0400

    fixed git

--- WorkWithFriends/EditProfileViewController.h
+++ WorkWithFriends/EditProfileViewController.h
@@ -7,7 +7,30 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "GlobalVariables.h"
 
 @interface EditProfileViewController : UIViewController
+{
+    NSString* firstName;
+    NSString* lastName;
+    NSString* aboutMe;
+    NSArray* mySkills;
+    UIImage* profilePicture;
+}
+
+- (NSString*) aboutMe;
+- (NSArray*) mySkills;
+- (NSString*) firstName;
+- (NSString*) lastName;
+- (UIImage*) profilePicture;
+- (void) setAboutMe:(NSDictionary*) me;
+- (void) setMySkills:(NSDictionary*) me;
+- (void) setFirstName:(NSDictionary*) me;
+- (void) setLastName:(NSDictionary*) me;
+- (void) setProfilePicture:(NSDictionary*) me;
+@property (weak, nonatomic) IBOutlet UIImageView *profilePictureLabel;
+@property (weak, nonatomic) IBOutlet UILabel *firstNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *lastNameLabel;
+@property (weak, nonatomic) IBOutlet UITextView *aboutMeLabel;
 
 @end
