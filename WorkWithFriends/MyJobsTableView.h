ad767008f35cca1a3ab65c4652bd2dd745c48d9b
Jeremy <jw2016@mit.edu>
2014-04-08 06:27:26 -0400

    changes to profile

---
+++ WorkWithFriends/MyJobsTableView.h
@@ -0,0 +1,13 @@
+//
+//  MyJobsTableView.h
+//  WorkWithFriends
+//
+//  Created by Jeremy Wohlwend on 4/8/14.
+//  Copyright (c) 2014 Jeremy Wohlwend. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyJobsTableView : UITableView
+
+@end
---
+++ WorkWithFriends/MyJobsTableView.m
@@ -0,0 +1,31 @@
+//
+//  MyJobsTableView.m
+//  WorkWithFriends
+//
+//  Created by Jeremy Wohlwend on 4/8/14.
+//  Copyright (c) 2014 Jeremy Wohlwend. All rights reserved.
+//
+
+#import "MyJobsTableView.h"
+
+@implementation MyJobsTableView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
